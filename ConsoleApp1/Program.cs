776466605b225a393f97cf506f1d2414037e26cb
mustafa <mustafa_yener05@hotmail.com>
2021-05-17 00:57:18 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
             int number = 2545;
             int price= 2545;
             float total = number + price;
+            var yener = "asdkljasdklasd";
 
             Console.WriteLine($"Hello World!   Total = {total}");
         }
